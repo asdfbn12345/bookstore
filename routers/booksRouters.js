56954c763c0a6130bb254ddb2d6a696404919795
asdfbn12345 <asdfbn12345@gmail.com>
2024-12-20 07:38:26 +0900

    fix: booksrouter typo

--- routers/booksRouters.js
+++ routers/booksRouters.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const { getBooksValidators } = require("../validations/bookValidator");
-const { getBookDetail } = require("../controllers/bookController");
+const { getBookDetail, getBooks } = require("../controllers/bookController");
 
-const booksRouter = express.Router();
+const router = express.Router();
 
 module.exports = booksRouter;
-
-module.exports = router;
 
 router.route("/").get(getBooksValidators, getBooks);
 router.route("/:id").get(getBookDetail);
